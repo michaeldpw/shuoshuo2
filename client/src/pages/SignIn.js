2e56325a1aa1b30d85803085170b7d47e96340b6
peiwendu <peiwendu@peiwendus-MacBook-Pro.local>
2019-09-22 10:22:06 +1000

    change redirect

--- client/src/pages/SignIn.js
+++ client/src/pages/SignIn.js
@@ -19,7 +19,7 @@ class SignIn extends React.Component{
     }
 
     handleClick = () => {
-        this.props.history.push('/signup');
+        this.props.history.push('https://reactmernstack1.herokuapp.com/#/signup');
         console.log('jump to signup');
     }
 
